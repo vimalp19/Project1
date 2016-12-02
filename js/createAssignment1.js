87f4118255d764d34d851f09b2d977867a4787aa
vimalp19 <vphb9@umsl.edu>
2016-12-01 18:59:10 -0600

    Update createAssignment1.js

--- js/createAssignment1.js
+++ js/createAssignment1.js
@@ -1,6 +1,6 @@
 jQuery(function($) {
 
-    .on('click', '#assignment', function() {
+    $('#assignment').on('click', '#assignmentForm', function() {
         
         var assignmentName = $('<div class="form-group" id="addAssignmentName"> <label class="col-xs-4 col-xs-offset-1 col-lg-2 col-lg-offset-3 control-label" for="assignmentName">Assignment Name:</label> <div class="col-xs-6 col-md-4 col-lg-3"> <input id="assignmentName" name="assignmentName" type="text" placeholder="Assignment Name" class="form-control input-md" required=""> </div> </div>');
         var assignmentDescription = $('<div class="form-group" id="addAssignmentDescription"> <label class="col-xs-4 col-xs-offset-1 col-lg-2 col-lg-offset-3 control-label" for="assignmentDescription">Description:</label> <div class="col-xs-6 col-md-4 col-lg-3"> <input id="assignmentDescription" name="assignmentDescription" type="text" placeholder="Assignment Description" class="form-control input-md" required=""> </div> </div>');
